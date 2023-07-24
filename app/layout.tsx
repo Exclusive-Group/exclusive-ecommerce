362a8709e46dc6c9aa759471f18e8f02f927c841
Heric Virgilio <heric.v.v.santos@gmail.com>
2023-07-24 17:32:58 -0300

    feat: added navbar component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import NavBar from './components/navbar'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={inter.className}>
+      <NavBar/>
         {children}
       </body>
     </html>
