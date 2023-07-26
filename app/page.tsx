68c43607a98ddc8be4ecad3c12dbd00087f6861c
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-26 14:25:15 -0300

    feat: new section for flash sales

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
+import Button from "./components/button";
 import NavMenu from "./components/navMenu";
 import Slider from "./components/slider";
+import SliderSection from "./components/sliderSection";
+import TagSection from "./components/tagSection";
 
 export default function Home() {
   return (
     <div className="px-[10%]">
       <div className="flex">
         <NavMenu />
-        <Slider/>
+        <Slider />
       </div>
+      <section id="flash_sales" className="my-36">
+        <TagSection name="Today's" />
+        <SliderSection name="Flash Sales" />
+        <div className="w-full flex justify-center mt-14">
+          <Button>
+            View All Products
+          </Button>
+        </div>
+      </section>
     </div>
   )
 }
