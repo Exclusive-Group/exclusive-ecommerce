b52171f72aad2fff23d3ac000788c22480ccd410
Heric Virgilio <heric.v.v.santos@gmail.com>
2023-07-26 14:36:18 -0300

    data: category list

---
+++ app/data/sliderCategory.ts
@@ -0,0 +1,52 @@
+export const listaCategory = [
+    {
+        name: "Phones",
+        url:"/iconPhone.png",
+        alt: "Phones"
+    },
+    {
+        name:"Computer",
+        url:"/iconComputer.png",
+        alt: "Computer"
+    },
+    {
+        name:"SmartWatch",
+        url: "/iconSmartWatch.png",
+        alt: "SmartWatch"
+    },
+    {
+        name:"Camera",
+        url: "/iconCamera.png",
+        alt: "Camera"
+    },
+    {
+        name:"HeadPhones",
+        url:"/iconHeadphone.png",
+        alt: "HeadPhones"
+    },
+    {
+        name:"Gaming",
+        url:"/iconGaming.png",
+        alt: "Gaming"
+    },
+    {
+        name: "Television",
+        url: "/iconTV.png",
+        alt: "Television"
+    },
+    {
+        name:"Drone",
+        url: "/iconDrone.png",
+        alt: "Drone"
+    },
+    {
+        name:"Security",
+        url: "/iconSeguranca.png",
+        alt: "Security"
+    },
+    {
+        name: "SmartHouse",
+        url:"/iconSmartHouse.png",
+        alt: "SmartHouse"
+    }
+]
