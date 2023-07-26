21340826c5b1f6252c604cda476dff4c68cfb1c8
Heric Virgilio <heric.v.v.santos@gmail.com>
2023-07-26 14:38:42 -0300

    feat: importation react

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import NavMenu from "./components/navMenu";
 import Slider from "./components/slider";
-
+import CategotySlider from "./components/categotySlider";
+import React from "react";
 export default function Home() {
   return (
     <div className="px-[10%]">
       <div className="flex">
-        <NavMenu />
+        <NavMenu/>
         <Slider/>
       </div>
+      <CategotySlider name={'Browse By Category'}/>
     </div>
   )
 }
