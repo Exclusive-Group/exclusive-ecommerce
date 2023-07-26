77afab99cf8dc7f88ca39fddbe603cf9bde25a65
Heric Virgilio <heric.v.v.santos@gmail.com>
2023-07-26 14:37:13 -0300

    data:rename index data for navMenu.ts

---
+++ app/data/navMenu.ts
@@ -0,0 +1,17 @@
+export const wonmansList = [
+    "Vestidos",
+    "Camisetas",
+    "Blusas e Camisas",
+    "Moletons",
+    "Conjuntos Femininos",
+    "Ternos e Blazers"
+]
+
+export const mansList = [
+    "Moletons e Agasalhos",
+    "Camisetas",
+    "Camisas",
+    "Bermudas Casuais",
+    "Conjuntos Masculinos",
+    "Jaquetas"
+]
