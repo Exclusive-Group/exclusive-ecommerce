4e9b781c696cbff4ed1b8c16542c6218bfc40f68
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-08-02 10:21:09 -0300

    feat: added footer component on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Button from "./components/button";
+import Footer from "./components/footer";
 import NavMenu from "./components/navMenu";
 import Slider from "./components/slider";
 import SliderSection from "./components/sliderSection";
@@ -6,20 +7,23 @@ import TagSection from "./components/tagSection";
 
 export default function Home() {
   return (
-    <div className="px-[10%]">
-      <div className="flex">
-        <NavMenu />
-        <Slider />
-      </div>
-      <section id="flash_sales" className="my-36">
-        <TagSection name="Today's" />
-        <SliderSection name="Flash Sales" />
-        <div className="w-full flex justify-center mt-14">
-          <Button>
-            View All Products
-          </Button>
+    <>
+      <div className="px-[10%]">
+        <div className="flex">
+          <NavMenu />
+          <Slider />
         </div>
-      </section>
-    </div>
+        <section id="flash_sales" className="my-36">
+          <TagSection name="Today's" />
+          <SliderSection name="Flash Sales" />
+          <div className="w-full flex justify-center mt-14">
+            <Button>
+              View All Products
+            </Button>
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </>
   )
 }
